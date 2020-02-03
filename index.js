06bedcedd3f1f2587608f879730518ebb1d99ac8
Taylor Wood <taylorwood39@gmail.com>
2020-02-03 13:46:15 -0600

    Add examples of Coercion.

--- index.js
+++ index.js
@@ -12,4 +12,11 @@ console.log(`This would be an example of Template Literals.`);
 console.log(`Hi, I am an example of ${term}.`);
 
 // COERCION is when JS will COERCE your data types into whatever if feels is appropriate.
-console.log("hello" + 22); // Exemplifies this
+console.log("hello" + 22); // Exemplifies this, is also Implicit Coercion
+console.log(Number(false)); // Exemplifies Explicit Coercion
+console.log(Boolean(222));
+
+console.log(parseInt("123.45"));
+console.log(parseFloat("123.45"));
+// Implicit Coercion, JS does what it thinks is best. It reads the code as it sees fit.
+// Explicit Coercion involves the user determining what should be done to the data.
