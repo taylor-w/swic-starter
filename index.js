a7df4a7f022c1ae305d18a7bead73afde0341f80
Taylor Wood <taylorwood39@gmail.com>
2020-02-03 13:18:50 -0600

    Add Notes and Example on Template Literals.

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
-// PRIMITIVE Data
-
-// We need a way to reuse or access data, so we create a REFERENCE.
-// 'const' alerts JS to reserve some space in memory
-// We provide a name for this space in memory
-// We use the ASSIGNMENT OPERATOR '='
-// This takes the VALUE on the right and assigns to the NAMED REFERENCE.
-
-const myString = "string"; // String data stored in memory using REFERENCE.
-let myNum = 24; // Number data with 'let' variable assignment. This allows reassignment.
-true; // Boolean Data - 1
-false; // Boolean Data - 0
-myNum = 8; // Reassignment of myNum let variable.
-
-// Creating variables for concatanation
-const name = "Taylor";
-let numOfYearsCoding = 1;
-
 // Should be pretty comfortable with what's going on here. Object(dot notation).method("argument")
 // Dot Notation opens up object and shows key of methods. Methods then invoke arguments.
 console.log("Hello World!");
 
-// First COMMIT HW
-// const theTruth =
-//   "On and off the court," +
-//   myNum +
-//   "and" +
-//   myNum(8) +
-//   "will always be remembered.";
-// true;
-// null;
-// undefined;
+// CONCATENATION OPERATOR is +. We use this to combine variables. Very familiar with this in C#
+const name = "Taylor";
+let numOfYearsCoding = 1;
+console.log(
+  "My name is " +
+    name +
+    " and I have been coding for " +
+    numOfYearsCoding +
+    " year(s)."
+);
+
+// TEMPLATE LITERALS which is the &{} and it makes it easier to do INTERPOLATION
+// Remember you must you tildas for Template Literals. `
+console.log(
+  `${name} is what my mother chose for me as a name when she thought I was going to be a girl.`
+);
