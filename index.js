5705073d49600dfc169ef072d91201ffad1b2792
Taylor Wood <taylorwood39@gmail.com>
2020-02-05 13:26:42 -0600

    Add Notes for Object

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
-const timeOfDay = "Good Morning, Vietnam!";
-const greeting = `Howdy Neighbors and ${timeOfDay}`;
-console.log(`${greeting}`);
+// console.log(something); // Is NOT in memory
+console.log(undefined); // Is a PRIMITIVE TYPE that signifies the absence of ANY value.
 
-let newVariable;
-console.log(typeof newVariable); // We should expect undefined variable.
-newVariable = "This variable was previously intentionally blank.";
-console.log(typeof newVariable); // We should expect new assignment to reflect string data type.
-// console.log(none); // 'none' is not a declared variable. Will result in broken execution.
-console.log(`${greeting}`); // Because reference in line 9 is not defined, code does not execute at all.
-const name = "Taylor";
-let favoriteSong = "SOMEBODY by IDK";
-let wage = 9.25;
-let age = 24;
-const onlyChild = false;
-let satisfied = false;
-let favoriteThing = "Life";
-console.log(typeof favoriteThing);
-favoriteSong = "BUSY by Saba";
-favoriteThing = 39;
-console.log(typeof favoriteThing);
-age = 25;
-wage = 25;
-satisfied = true;
-console.log(
-  `${name}'s football number was ${favoriteThing}, and would love to work for ${wage} an hour.`
-);
-console.log(
-  "This is an example of concatenation using " + "the plus sign operator."
-);
+let x;
+console.log(x); // Example of being undefined. We have the variable, but not assignment.
+// NULL represents a deliberate assignment of 'nothing.' It is also a PRIMITIVE type.
+x = null;
+
+// THIS is a an OBJECT assigned to a const variable, 'me.'
+const me = {
+  name: "Taylor",
+  age: 24,
+  pets: {
+    dog1: "Guiness",
+    dog2: "Indie"
+  }
+};
+console.log(me.name);
+// Everything that's not a PRIMITIVE data type, is an OBJECT. This OBJECT structure is why .(dot) notation is so prevalent in the JS language.
+
+// Create a new key value pair with age = 23.
+// me.age = 23;
+// console.log(me.age);
+
+console.log(me.pets.dog2);
