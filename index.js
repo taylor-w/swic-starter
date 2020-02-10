ef3aa4699e933807cf3f0429d632e9b4b0e6e749
Taylor Wood <taylorwood39@gmail.com>
2020-02-10 13:15:01 -0600

    Add Examples of Bracket Notation

--- index.js
+++ index.js
@@ -19,3 +19,10 @@ console.log(bio.age + bio.weight);
 bio.hair = "blonde";
 console.log(bio.hair);
 console.log(bio.addFiveYears(bio.age));
+
+console.log(bio.name.fName); // This is an example of dotNotation
+console.log(bio["name"]); // This is an example of bracketNotation - MUST have quotations if        referencing key within object.
+
+const NewKey = "age";
+
+console.log(bio[NewKey]); // This is interesting. We are using bracket notation to utilize our new const in order to search within our object. So our log is looking for our bio object, then using our new const, 'NewKey' which is "age" and looks for "age" within bio.
