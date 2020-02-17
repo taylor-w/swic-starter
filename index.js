5549bf49f332ab92768fa3274c8647b7ffa34a6a
Taylor Wood <taylorwood39@gmail.com>
2020-02-17 13:14:39 -0600

    Add Examples of Logical Operators

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// The dynamic conversion of data is helpful... example:
-const name = "";
-
-// quickly check to see if a value is 'truthy' or 'falsey'
-// For example, if I left the 'name' blank...
-if (name) {
-  console.log("User has entered value for name field");
-} else {
-  console.error("User has left name field blank");
+const x = 3;
+// So we gave X the value of 3. The modulus of that variable, x, or 3, is 1. So 1 is === 1.
+// if (x % 2 === 1) {
+//   console.log("x is odd because it has a remainder of 1.");
+// }
+// Logical Operators
+// Check if X is ODD and IF it's less than 10.
+if (x % 2 === 1 && x < 10) {
+  console.log("x is odd and x is less than 10.");
 }
