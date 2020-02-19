492683cf2f08e4dd46d471435434126b1d56aa93
Taylor Wood <taylorwood39@gmail.com>
2020-02-19 13:31:13 -0600

    Add Unary Operator and Optimized FizzBuzz!

--- index.js
+++ index.js
@@ -9,16 +9,28 @@ console.log(sum);
 
 let num = 1;
 let total = 100;
+// (!0) === True
 while (num <= total) {
-  if (num % 3 === 0 && num % 5 === 0) {
+  if (!(num % 3) && !(num % 5)) {
     console.log("FizzBuzz!");
-  } else if (num % 3 === 0) {
+  } else if (!(num % 3)) {
     console.log("Fizz!");
-  } else if (num % 5 === 0) {
+  } else if (!(num % 5)) {
     console.log("Buzz!");
   } else {
     console.log(num);
   }
   num += 1;
 }
+console.log(Boolean(0));
+console.log(Boolean(1));
 // Examples of While loops. Notice Initializing, Conditional placements, and Incremention.
+/**
+ * '!' is the NOT operator. It's a UNARY OPERATOR.
+ * Using this in front of any data type (such as a number)
+ * will immediately convert that data type into a Boolean.
+ * Then, that value will be inversed as per the NOT operator.
+ */
+console.log(!0);
+console.log(!"hello");
+console.log(!"");
