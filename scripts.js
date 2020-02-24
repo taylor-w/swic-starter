b93ec027b1b2fb90fb38aff997a8dbbdedbcca3d
Taylor Wood <taylorwood39@gmail.com>
2020-02-24 13:22:54 -0600

    Add Example of Prompt for MidTerm Project

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-console.log("Hello from JS!");
+const gameInfo = {};
+
+gameInfo.title = prompt("What is the title of your game?");
+if ((gameInfo.title = "Halo Reach")) {
+  gameInfo.map = "Zealot, Countdown, Forge World";
+}
+gameInfo.map = prompt("Which map are you playing?");
+if ((gameInfo.map = "Zealot")) {
+  gameInfo.mode = "Capture the Flat, Team Slayer";
+}
