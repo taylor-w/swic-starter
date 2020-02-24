f442eb17be146a98b185054a023c0d190c770b93
Taylor Wood <taylorwood39@gmail.com>
2020-02-24 13:42:53 -0600

    Add FOR Loop Examples

--- scripts.js
+++ scripts.js
@@ -1,10 +1,23 @@
-const gameInfo = {};
-
-gameInfo.title = prompt("What is the title of your game?");
-if ((gameInfo.title = "Halo Reach")) {
-  gameInfo.map = "Zealot, Countdown, Forge World";
+// Log the numbers from 1 to 10.
+// FOR limits use of local variable
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
 }
-gameInfo.map = prompt("Which map are you playing?");
-if ((gameInfo.map = "Zealot")) {
-  gameInfo.mode = "Capture the Flat, Team Slayer";
+// Log Fizz, Buzz, FizzBuzz!
+for (let i = 0; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz!");
+  } else if (i % 3 === 0) {
+    console.log("Fizz!");
+  } else if (i % 5 === 0) {
+    console.log("Buzz!");
+  } else {
+    console.log(i);
+  }
+}
+// Log Summonation
+let total = 0;
+for (let i = 1; i <= 10; i++) {
+  total += i;
+  console.log(total);
 }
