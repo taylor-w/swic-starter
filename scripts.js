ed23deacea3f527b9b31d9c4acf12e714c9c6d75
Taylor Wood <taylorwood39@gmail.com>
2020-02-26 01:03:09 -0600

    Add FIZZBUZZ For and Ex. Window.Confirm

--- scripts.js
+++ scripts.js
@@ -1,23 +1,28 @@
-// Log the numbers from 1 to 10.
-// FOR limits use of local variable
-for (let i = 1; i <= 10; i++) {
-  console.log(i);
-}
-// Log Fizz, Buzz, FizzBuzz!
+// Fizz! Buzz! FizzBuzz!
 for (let i = 0; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
+  if (!(i % 3) && !(i % 5)) {
     console.log("FizzBuzz!");
-  } else if (i % 3 === 0) {
+  } else if (!(i % 3)) {
     console.log("Fizz!");
-  } else if (i % 5 === 0) {
+  } else if (!(i % 5)) {
     console.log("Buzz!");
   } else {
     console.log(i);
   }
 }
-// Log Summonation
-let total = 0;
-for (let i = 1; i <= 10; i++) {
-  total += i;
-  console.log(total);
-}
+// Empty Object
+const emptyObject = {};
+
+// Object is empty, so 'isConfirmed' logs back 'undefined.'
+// Now, we use Window object's method 'confirm.'
+// IF user selects OK, then isConfirmed received boolean response of TRUE, which makes it exist within 'emptyObject.'
+// ELSE user selects CANCEL, so FALSE is filled within isConfirmed key instead, which still makes it exist within 'emptyObject,' but with a different value.
+
+emptyObject.isConfirmed = window.confirm();
+
+// Logging value of emptyObject
+console.log(emptyObject);
+// Logging value of isConfirmed key within emptyObject
+
+// We need to be in browser to recognize global object, 'Window,' so we receive undefined.
+// References afterwards never exist since isConfirmed key never received value.
