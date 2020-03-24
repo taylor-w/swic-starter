cd3605a3a61ce80a6d577c052fb03545c8646639
Taylor Wood <taylorwood39@gmail.com>
2020-03-24 12:23:11 -0500

    Add Further Argument Detail

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
-const sayHello = function sayHello() {
+const sayHello = function() {
   // Let's 'log' if we received any arguments.
-  console.log(arguments);
+  console.log(`${arguments["0"]} ${arguments["4"]}`);
 };
 
 /* Example of passing arguments into function. */
-sayHello("hello", "by", "hi", "world");
+sayHello("hello", "by", "hi", "world", "Taylor");
 
 /*Keyword argument is used to show number of arguments within function.
 Notice the number of arguments starts with number 0 when logged. */
+
+//You have to use bracket notation with keyword arguments within scope of function.
