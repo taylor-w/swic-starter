4494dc34fa000ece368949d6e080c7d0345d1a08
Taylor Wood <taylorwood39@gmail.com>
2020-03-24 12:06:56 -0500

    Add Example of Simple Function

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
-let game = prompt("What is your age?");
+// FUNCTION look a lot like patterns seen with 'for', 'while', 'if'...
+const logLongMsg = function() {
+  console.log(
+    "Hello! Don't mind me, just logging a long message with the help of a function!"
+  );
+};
 
-age = Number(age);
+logLongMsg();
 
-if (!age) {
-  alert("This is not a number!");
-}
+/* Take note of the syntax, which should look familiar. Also notice
+the way in which you call the function. It looks as to be expected based
+upon previous programing knowledge. */
