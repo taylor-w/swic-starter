04c4e791d5a2bb49bec0581c4e652aae804281da
Taylor Wood <taylorwood39@gmail.com>
2020-03-31 15:39:41 -0500

    Add Arrow Syntax Notes

--- scripts.js
+++ scripts.js
@@ -1,32 +1,33 @@
-// TODO: Example of Summation using a function that requires a start and end point.
-// TODO: Homework, add increment parameter.
+// ARROW SYNTAX
 
-/* This function is declared as 'summation' with 3 parameters.
-Parameter 1, 'start' the number to start counting from.
-Parameter 2, 'end' the number to stop count at when reached.
-Parameter 3, 'increment' the number we increment by each time when counting. */
+// Function Declaration
+function card(age) {}
 
-// Function
-function summation(start, end, increment) {
-  // A variable to hold our 'total' that we will add the sum to and return.
-  let total = 0;
-  /* We do not have to reinitialize 'increment' for it's receiving it's value
-  from the passing of an argument. We are going to count by 'increment' number, starting with
-  'start' value passed by argument. As long as that 'start' number is less than or equal
-  to end, this is our condition, we will count by 'increment.' As we add 'increment' value
-  to 'start' value, 'start' value changes each loop, we also take 'total' and sum each iteration
-  of 'start' value. */
-  for (increment; start <= end; start += increment) {
-    /* Log start through each loop to see the 'start' value
-    and how it changes through each condition check. While that
-    start counts to the 'end' point, we 'store' and add each value
-    of 'start' to 'total'. */
-    console.log(start);
-    total += start;
-  }
-  /* Loop is finished, we will now RETURN the 'total' of all the numbers within the range 'start - end'. Essentially, ever number that appears within the condition check that we saw while counting by 'increment' will be summed and returned. */
-  return total;
-}
+// Function Expression
+const card2 = function card(age) {
+  age >= 21;
+};
 
-// Log Function
-console.log(summation(1, 10, 4));
+// Arrow Syntax
+const cardArrow = age => age >= 21;
+/* This arrow syntax allows one line code when there is only a single
+statement in the function. */
+
+/* Notice how the arrow => replaces the function keyword */
+/* It's also important to note that the RETURN keyword is even more important than
+before when using arrow syntax. Without that RETURN keyword, nothing is returned, so
+the logged results are 'undefined'. */
+
+const adder = (num1, num2) => {
+  num1 + num2;
+};
+const correctAdder = (num1, num2) => {
+  return num1 + num2;
+};
+
+console.log(cardArrow(11));
+console.log(adder(2, 3));
+console.log(correctAdder(2, 3));
+
+/* The Arrow syntax requires less keystrokes, is up-to-date with ES2015, and is
+more importantly, MR.Cleaaaan. */
