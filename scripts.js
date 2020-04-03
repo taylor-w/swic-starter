6048d429f9818f58b27fb0d1cba6845834478b35
Taylor Wood <taylorwood39@gmail.com>
2020-04-03 14:45:33 -0500

    Add Prototypes

--- scripts.js
+++ scripts.js
@@ -1,25 +1,42 @@
-// Constructor Functions
+// Prototypes
 
-/* This isn't the first time we've come across the 'constructor' word in
-programming. C# has it, when you create a new instance of some object, and
-it seems to be used likewise here in Javascript.*/
+/* We use PROTOTYPES to extract a collection of properties and
+methods for reuse within specific instances. This leads to the
+main purpose of Object Oriented Programming. Javascript handles
+OOP with Prototypes. */
 
+// Constructor Function with some properties removed:
 function Person(fname, lname) {
   this.fname = fname;
   this.lname = lname;
-
-  this.getFullName = function() {
-    return `${fname} ${lname}`;
-  };
 }
-console.log(Person);
-/* At this point, it's just a function. It's being set up to create or
-CONSTRUCT new objects */
+
+/* We use the Person Constructor in conjunction with the PROTOTYPE
+keyword to share the properties of our Constructor Function */
+Person.prototype.getFullName = function() {
+  return `${this.fname} ${this.lname}`;
+};
 
 const taylor = new Person("Taylor", "Wood");
+const eddie = new Person("Eddie", "Murphy");
 console.log(taylor);
-/* Now we have create a new INSTANCE of the Person function with the keyword 'new'.
-Notice the log of 'taylor'. It shows the same format of contents within 'Person',
-but grabbed the values provided from 'taylor' because the 'this' keyword within the
-constructor function. */
+/* At this point, it's similar to our Constructor Function notes, but
+we haven't copied the getFullName function into every NEW instance.
+This saves memory! */
+
 console.log(taylor.getFullName());
+/* Now we can access the Prototype Function on a need basis to save
+memory, and make unique use of the Prototype Function as needed.
+This, is the beauty of Object Oriented Programming. The need and ease
+of access, to better serve our programming/design needs. */
+taylor.getFullName = function() {
+  return "Woody - There's a snake in my boot!";
+};
+console.log(taylor.getFullName());
+console.log(eddie.getFullName());
+
+/* We used the Prototype function three times in our log statements.
+And even changed it as desired for 'taylor', but kept it's original
+assignment and use for 'eddie'. Our Prototype Function has a 'designed'
+purpose, but can change on case to case assignment. NOTE: Don't lose
+track of assignment - this is Javascript's duality.*/
