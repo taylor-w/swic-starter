c324b027d69d7676227dbeec13cc6ac75d9533c8
Taylor Wood <taylorwood39@gmail.com>
2020-04-03 14:07:20 -0500

    Add Functions Become Methods

--- scripts.js
+++ scripts.js
@@ -1,51 +1,49 @@
-// Final Function Thoughts and Homework
+// Functions Becoming Methods
 
-// Function Declaration
-function sayingGenerator() {
-  const phrase = "Heeeeey, it's the Fonz."; // Locally scoped variable
-  const p = "heck";
-  console.log(p);
-  return phrase;
-}
+/* A function in the global scope is a function, but becomes a method when
+scoped within an object. */
 
-// console.log(phrase); // Error because we are trying to pull a local variable of 'sayingGenerator()' to a global setting.
-
-const saying = sayingGenerator();
-// saying(); // Will error because function is not a function. No need to invoke();
-saying;
-console.log(saying);
-
-function valueLogger(value) {
-  console.log(value);
-}
-
-valueLogger(); // Undefined because no argument is being passed.
-
-valueLogger("Howdy ho, neighborino!");
-
-valueLogger(3 + 7); // Will actually give value
-
-valueLogger(3, 7);
-
-function doubler(num) {
-  return num * 2;
-}
-
-const shouldBeTen = doubler(5);
-console.log(shouldBeTen);
-
-function doubleValueLogger(value1, value2) {
-  console.log(value1, value2);
-}
-
-doubleValueLogger("hello", "how are you");
-
-doubleValueLogger("hello");
-
-function add(num1, num2) {
+// Example
+function adder(num1, num2) {
   return num1 + num2;
 }
+console.log(`This is a function ${adder(3, 2)}`);
 
-const sum = add(7, 12);
+const basicMath = {
+  subtractor: function(num1, num2) {
+    return num1 - num2;
+  }
+};
+console.log(`This is a method ${basicMath.subtractor(3, 2)}`);
 
-console.log(sum);
+/* Some syntax changes when functions become methods, but the process and rules of objects
+are the same as before. How we reference methods within objects, how they make copies of
+primitive data but make reference copies of objects. Nearly every rule as we know it to this
+juncture, still holds water. */
+
+/* NOTE: 'this' is a keyword that references another property within an object with that property
+is going to be used by a method within the SAME object. With other languages and how we've been exposed to keyword 'this', it makes total sense. */
+
+// Example
+const me = {
+  age: 24,
+  weight: 175,
+  hair: "brown",
+  getOlder: function(years) {
+    return this.age + years;
+  },
+  getYounger: function(years) {
+    return this.age - years;
+  },
+  gainWeight: function(weight) {
+    return this.weight + weight;
+  },
+  loseWeight: function(weight) {
+    return this.weight - weight;
+  }
+};
+console.log(me.getOlder(5));
+console.log(me.gainWeight(10));
+console.log("Found the Fountain of Youth");
+console.log(me.getYounger(2));
+console.log(me.loseWeight(5));
