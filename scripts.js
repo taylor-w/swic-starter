8e777162df8bbab45407fc52eb584f3ed3bca583
Taylor Wood <taylorwood39@gmail.com>
2020-04-07 14:48:34 -0500

    Add Array Pop Method

--- scripts.js
+++ scripts.js
@@ -11,3 +11,13 @@ mapCallouts.push("gold one"); // PUSH element into array
 console.log(mapCallouts);
 // Check value RETURN from PUSH
 console.log(mapCallouts.push("gold one")); // RETURNS Array.length
+
+// POP METHOD
+// Log
+console.log(mapCallouts);
+// Array Prototype Function: POP
+mapCallouts.pop(); // POP removes last element from array
+// Log
+console.log(mapCallouts);
+// Check value RETURN from POP
+console.log(mapCallouts.pop()); // RETURNS Element it Removes
