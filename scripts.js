71aeb25c4b13a87d6f565e086993fae38d4bb7f5
Taylor Wood <taylorwood39@gmail.com>
2020-04-07 18:16:38 -0500

    Add Pseudo Code for Craps

--- scripts.js
+++ scripts.js
@@ -14,4 +14,21 @@ function diceRoll(diceNum = 2, diceSides = 6) {
   return roll;
 }
 
+// Craps game
+
+// Use bets for pass => roll will total 7 or 11
+
+// UpperBound: 12
+// IF 2, 3, 12 => bet is outright lost
+// ELSE IF 7, 11 => bet is outright won
+// ELSE => 1, 4, 5, 6, 8, 9, 10
+// MARK one of ELSE numbers
+// LowerBound: 1
+
+// Start Loop
+// IF roll hits MARK => WIN
+// ELSE roll until MARK hit || 7 => LOSS
+// End Loop
+
+// Reward/Take 'bet' based on game outcome
 console.log(diceRoll(2, 6));
