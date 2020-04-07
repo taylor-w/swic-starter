92cd97a1c76c28bfc3cb31b663c797abf93c8523
Taylor Wood <taylorwood39@gmail.com>
2020-04-07 17:54:39 -0500

    Add Functions for diceRoll

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
 // 3/3 - 5pts
+function getRanIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function diceRoll(diceNum = 2, diceSides = 6) {
+  let roll = [];
+  for (let i = 0; i < diceNum; i++) {
+    roll.push(getRanIntInclusive(1, diceSides));
+  }
+
+  return roll;
+}
+
+console.log(diceRoll(2, 6));
