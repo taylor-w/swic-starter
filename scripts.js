d93f3af5a8583c37dba0b82ec60d5a4e5cfe98f3
Taylor Wood <taylorwood39@gmail.com>
2020-04-07 21:03:29 -0500

    Add Properties to Object and Display Results

--- scripts.js
+++ scripts.js
@@ -3,10 +3,13 @@
 const crapsRolls = {
   // Winner Values
   win: [7, 11],
+  winValue: 0,
   // Loser Values
   loss: [2, 3, 12],
+  loseValue: 0,
   // On Values
   on: [1, 4, 5, 6, 8, 9, 10],
+  onValue: 0,
   numOfRolls: 0
 };
 
@@ -16,11 +19,13 @@ function getRanIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function firstRoll(diceNum = 2, diceSides = 6) {
+function diceRoll(diceNum = 2, diceSides = 6) {
   let roll = [];
   for (let i = 0; i < diceNum; i++) {
     roll.push(getRanIntInclusive(1, diceSides));
   }
+  // Count Number of Rolls
+  crapsRolls.numOfRolls++;
   // Two Dice Rolled
   let die1 = roll[0];
   let die2 = roll[1];
@@ -28,25 +33,40 @@ function firstRoll(diceNum = 2, diceSides = 6) {
   let value = die1 + die2;
   // Player Rolls Winning Value
   if (crapsRolls.win.includes(value)) {
-    let winValue = value;
-    return winValue;
+    crapsRolls.winValue = value;
+    return crapsRolls.winValue;
     // Player Rolls Losing Value
   } else if (crapsRolls.loss.includes(value)) {
-    let loseValue = value;
-    return loseValue;
+    crapsRolls.loseValue = value;
+    return crapsRolls.loseValue;
     // Player Rolls Again
   } else {
     let ranIndex = getRanIntInclusive(0, 6);
     // 'on' Value Gets Randomly Selected from Possible 'on' Values
-    let onValue = crapsRolls.on[ranIndex];
-    return onValue;
+    crapsRolls.onValue = crapsRolls.on[ranIndex];
+    return crapsRolls.onValue;
   }
 }
 
 // Game Results
-console.log(firstRoll());
-
-// Craps game
+if (crapsRolls.win.indexOf(diceRoll()) !== -1) {
+  console.log(
+    `Player WON on dice roll number ${crapsRolls.numOfRolls}` +
+      ` with the winning roll value of ${crapsRolls.winValue}`
+  );
+} else if (crapsRolls.loss.indexOf(diceRoll()) !== -1) {
+  console.log(
+    `Player LOST on dice roll number ${crapsRolls.numOfRolls}` +
+      ` with the losing roll value of ${crapsRolls.loseValue}`
+  );
+} else {
+  console.log(
+    `Player rolled ${crapsRolls.onValue}` +
+      ` on dice roll number ${crapsRolls.numOfRolls}.` +
+      ` Dealer marked ${crapsRolls.onValue} as the point number. Player must roll again to potentially win.`
+  );
+  diceRoll();
+}
 
 // Use bets for pass => roll will total 7 or 11
 
