a7088c706e68ba96c42b0517cbe935cac60363f3
Taylor Wood <taylorwood39@gmail.com>
2020-04-07 20:33:46 -0500

    Add crapsRolls Object

--- scripts.js
+++ scripts.js
@@ -1,4 +1,15 @@
 // 3/3 - 5pts
+// Craps Object
+const crapsRolls = {
+  // Winner Values
+  win: [7, 11],
+  // Loser Values
+  loss: [2, 3, 12],
+  // On Values
+  on: [1, 4, 5, 6, 8, 9, 10],
+  numOfRolls: 0
+};
+
 function getRanIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -15,29 +26,24 @@ function firstRoll(diceNum = 2, diceSides = 6) {
   let die2 = roll[1];
   // Total of Two Dice Rolled
   let value = die1 + die2;
-  // Winner Values
-  let win = [7, 11];
-  // Loser Values
-  let loss = [2, 3, 12];
-  // Marked 'on'
-  let on = [1, 4, 5, 6, 8, 9, 10];
   // Player Rolls Winning Value
-  if (win.includes(value)) {
+  if (crapsRolls.win.includes(value)) {
     let winValue = value;
     return winValue;
     // Player Rolls Losing Value
-  } else if (loss.includes(value)) {
+  } else if (crapsRolls.loss.includes(value)) {
     let loseValue = value;
     return loseValue;
     // Player Rolls Again
   } else {
     let ranIndex = getRanIntInclusive(0, 6);
     // 'on' Value Gets Randomly Selected from Possible 'on' Values
-    let onValue = on[ranIndex];
+    let onValue = crapsRolls.on[ranIndex];
     return onValue;
   }
 }
-/* TEST */
+
+// Game Results
 console.log(firstRoll());
 
 // Craps game
